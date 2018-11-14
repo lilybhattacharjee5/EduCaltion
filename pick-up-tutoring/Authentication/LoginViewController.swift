93b46505000592f2a2b3c23db3e595cb1b836f70
sayanchat <sayanc00@gmail.com>
2018-11-14 07:34:26 -0800

    added authentication

--- pick-up-tutoring/Authentication/LoginViewController.swift
+++ pick-up-tutoring/Authentication/LoginViewController.swift
@@ -7,29 +7,75 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class LoginViewController: UIViewController {
+class LoginViewController: UIViewController, UITextFieldDelegate {
 
+    var username = ""
+    var password = ""
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var usernameTF: UITextField!
+    
+    @IBAction func loginButton(_ sender: Any) {
+        guard let userText = usernameTF.text else { return }
+        guard let passText = passwordTF.text else { return }
+        
+        if userText == "" || passText == "" {
+            let alertController = UIAlertController(title: "Login Error", message: "Please enter a username and password.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            self.present(alertController, animated: true, completion: nil)
+        }
+        else {
+            Auth.auth().signIn(withEmail: userText, password: passText) { (user, error) in
+                if error != nil {
+                    let alertController = UIAlertController(title: "Log In Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                } else {
+                    self.performSegue(withIdentifier: "loginToMain", sender: self)
+                }
+            }
+        }
+    }
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        performSegue(withIdentifier: "loginToSignUp", sender: self)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.usernameTF.delegate = self
+        self.passwordTF.delegate = self
         // Do any additional setup after loading the view.
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        /*Auth.auth().addStateDidChangeListener { (auth, user) in
+            if user != nil {
+                self.performSegue(withIdentifier: "loginToMain", sender: self)
+            }
+        }*/
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
+        
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == self.usernameTF {
+            if textField.text != nil {
+                self.username = textField.text!
+            }
+        } else {
+            if textField.text != nil {
+                self.password = textField.text!
+            }
+        }
     }
-    */
+    
 
 }
--- pick-up-tutoring/Authentication/SignUpViewController.swift
+++ pick-up-tutoring/Authentication/SignUpViewController.swift
@@ -7,12 +7,58 @@
 //
 
 import UIKit
+import FirebaseAuth
 
-class SignUpViewController: UIViewController {
+class SignUpViewController: UIViewController, UITextFieldDelegate {
 
+    @IBOutlet weak var usernameTF: UITextField!
+    @IBOutlet weak var passwordTF: UITextField!
+    @IBOutlet weak var emailTF: UITextField!
+    
+    var email = ""
+    var username = ""
+    var password = ""
+    
+    @IBAction func signUpButton(_ sender: Any) {
+        guard let emailText = emailTF.text else { return }
+        guard let passwordText = passwordTF.text else { return }
+        guard let usernameText = usernameTF.text else { return }
+        
+        if emailText == "" || passwordText == "" || usernameText == "" {
+            let alertController = UIAlertController(title: "Form Error.", message: "Please fill in form completely.", preferredStyle: .alert)
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            present(alertController, animated: true, completion: nil)
+        } else {
+            Auth.auth().createUser(withEmail: emailText, password: passwordText) { (user, error) in
+                if error == nil {
+                    let changeRequest = Auth.auth().currentUser?.createProfileChangeRequest()
+                    changeRequest?.displayName = usernameText
+                    changeRequest?.commitChanges { (error) in
+                        if error == nil  {
+                            let accountCreatedController = UIAlertController(title: "Success.", message: "The account has been created.", preferredStyle: .alert)
+                            let successAction = UIAlertAction(title: "OK.", style: .default) { (action:UIAlertAction) in
+                                self.performSegue(withIdentifier: "signUpToLogin", sender: self)
+                            }
+                            accountCreatedController.addAction(successAction)
+                            self.present(accountCreatedController, animated: true, completion: nil)
+                        }
+                    }
+                } else {
+                    let alertController = UIAlertController(title: "Sign Up Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        self.usernameTF.delegate = self
+        self.emailTF.delegate = self
+        self.passwordTF.delegate = self
         // Do any additional setup after loading the view.
     }
 
@@ -21,15 +67,19 @@ class SignUpViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        if textField == self.usernameTF {
+            if textField.text != nil {
+                self.username = textField.text!
+            }
+        } else if textField == self.passwordTF {
+            if textField.text != nil {
+                self.password = textField.text!
+            }
+        } else {
+            if textField.text != nil {
+                self.email = textField.text!
+            }
+        }
     }
-    */
-
 }
